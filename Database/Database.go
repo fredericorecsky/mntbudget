40b954ae706c0232d8fc9b18c14bf2bef6466f76
Frederico Recsky <frederico.recsky@booking.com>
2018-11-19 08:08:41 +0100

    Connect to database local/appengine/appengine dev

--- Database/Database.go
+++ Database/Database.go
@@ -1,15 +1,41 @@
 package Database
 
 import (
-	_ "github.com/go-sql-driver/mysql"
 	"database/sql"
+	"fmt"
+	"google.golang.org/appengine"
 	"log"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	_ "google.golang.org/appengine"
 )
 
-func Database() (*sql.DB, error){
-	db, err := sql.Open( "mysql", "user:password@tcp(ipaddress:port)/mntbudget")
+func Database() (*sql.DB, error) {
+	var (
+		//local_proxy= os.Getenv("CLOUDSQL_PROXY")
+		//local_proxy= os.Getenv("CLOUDSQL_PROXY")
+		connectionName= os.Getenv("CLOUDSQL_CONNECTION_NAME")
+		user= os.Getenv("CLOUDSQL_USER")
+		password= os.Getenv("CLOUDSQL_PASSWORD") // NOTE: password may be empty
+		dsn string
+	)
 
-	if ( err != nil ){
+	cloudengine :=  appengine.IsDevAppServer()
+	appserver := appengine.IsAppEngine()
+
+	if ! appserver || cloudengine  {
+		dsn = fmt.Sprintf("%s:%s@tcp(%s)/%s",
+				user,
+				password,
+				connectionName,
+				"mntbudget")
+	} else {
+		dsn = fmt.Sprintf("%s:%s@cloudsql(%s)/", user, password, connectionName)
+	}
+	db, err := sql.Open( "mysql", dsn)
+
+	if err != nil {
 		log.Fatal(err)
 	}
 
